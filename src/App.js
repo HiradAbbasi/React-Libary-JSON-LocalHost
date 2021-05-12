c31085012ce6b303484b845def13b49c8328b6db
Hirad Abbasi <hiradcana@gmail.com>
2021-05-11 19:11:30 -0500

    Optimizing code(fixing names, indentation, spacing, functions)

--- src/App.js
+++ src/App.js
@@ -4,178 +4,146 @@ import Book from './Book'
 import './App.css';
 
 const App = () => {
-    const [books, setBooks] = useState([]);
-    const [filterInput, setFilterInput] = useState("");
+  const [books, setBooks] = useState([]);
+  const [filterInput, setFilterInput] = useState("");
 
-    useEffect(() => {
-      const fetchData = async() => {
-        const response = await fetch(`http://localhost:5000/books`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        const books = await response.json();
-        setBooks(books.filter(el => el.imageLinks !== undefined && el.authors !== undefined));
-      }
-
-      fetchData();
-    }, []);
-
-    const updateFilterInput = (e) => {
-      setFilterInput(e.target.value);
-    }
-
-    const changeState = async(value, id) => {
-      setBooks(currentState => {
-        return currentState.map(
-          book => book.id === id ? {...book, currentState: value } : book
-        )
-      });
-
-      const res = await fetch(`http://localhost:5000/books/${id}`, {
-        method: "PUT",
+  useEffect(() => {
+    const fetchData = async() => {
+      const response = await fetch(`http://localhost:5000/books`, {
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({...books.find(el => el.id === id), "currentState": value })
       });
-      const result = await res.json();
-      console.log(result)
+      const books = await response.json();
+      setBooks(books.filter(el => el.imageLinks !== undefined && el.authors !== undefined));
     }
 
-    return ( <
-      > { /* <Header /> */ } <
-      main >
-      <
-      Switch >
-      <
-      Route exact path = "/" >
-      <
-      div className = "app" >
-      <
-      div className = "list-books" >
-      <
-      div className = "list-books-title" > < h1 > MITTReads < /h1></div >
-      <
-      div className = "list-books-content" >
-      <
-      div >
-      <
-      div className = "bookshelf" >
-      <
-      h2 className = "bookshelf-title" > Currently Reading < /h2> <
-      div className = "bookshelf-books" >
-      <
-      ol className = "books-grid" > {
-        books.filter(el => el.currentState === "currentlyReading").map((book, index) => ( <
-          Book currentState = { book.currentState }
-          title = { book.title }
-          authors = { book.authors }
-          image = { book.imageLinks }
-          id = { book.id }
-          key = { index }
-          changeState = { changeState }
-          />
-        ))
-      } <
-      /ol> <
-      /div> <
-      /div> <
-      div className = "bookshelf" >
-      <
-      h2 className = "bookshelf-title" > Want To Read < /h2> <
-      div className = "bookshelf-books" >
-      <
-      ol className = "books-grid" > {
-        books.filter(el => el.currentState === "wantToRead").map((book, index) => ( <
-          Book currentState = { book.currentState }
-          title = { book.title }
-          authors = { book.authors }
-          image = { book.imageLinks }
-          id = { book.id }
-          key = { index }
-          changeState = { changeState }
-          />
-        ))
-      } <
-      /ol> <
-      /div> <
-      /div> <
-      div className = "bookshelf" >
-      <
-      h2 className = "bookshelf-title" > Read < /h2> <
-      div className = "bookshelf-books" >
-      <
-      ol className = "books-grid" > {
-        books.filter(el => el.currentState === "read").map((book, index) => ( <
-          Book currentState = { book.currentState }
-          title = { book.title }
-          authors = { book.authors }
-          image = { book.imageLinks }
-          id = { book.id }
-          key = { index }
-          changeState = { changeState }
-          />
-        ))
-      } <
-      /ol> <
-      /div> <
-      /div> <
-      /div> <
-      /div>
+    fetchData();
+  }, []);
 
-      <
-      div className = "open-search" > < Link to = "/search"
-      className = 'close-search' > Add a book < /Link></div >
-      <
-      /div> <
-      /div> <
-      /Route> <
-      Route exact path = "/search" >
-      <
-      div className = "app" >
-      <
-      div className = "search-books" >
-      <
-      div className = "search-books-bar" >
-      <
-      Link to = "/"
-      className = 'close-search' > Close < /Link> <
-      div className = "search-books-input-wrapper" >
-      <
-      input className = 'search-contacts'
-      type = 'text'
-      placeholder = "Search by title or author"
-      value = { filterInput }
-      onChange = { updateFilterInput }
-      /> <
-      /div> <
-      /div> <
-      div className = "search-books-results" > {
-        filterInput && < div className = "results-quantity" > Your search returned { `${books.filter(el => el.title.toLowerCase().includes(filterInput.toLowerCase()) || el.authors.join().toLowerCase().includes(filterInput.toLowerCase())).length} ` }
-        results. < /div>} <
-        ol className = "books-grid" > {
-          filterInput && books.filter(el => el.title.toLowerCase().includes(filterInput.toLowerCase()) || el.authors.join().toLowerCase().includes(filterInput.toLowerCase())).map((book, index) => ( <
-            Book currentState = { book.currentState }
-            title = { book.title }
-            authors = { book.authors }
-            image = { book.imageLinks }
-            id = { book.id }
-            key = { index }
-            changeState = { changeState }
-            />
-          ))
-        } <
-        /ol> <
-        /div> <
-        /div> <
-        /div> <
-        /Route> <
-        /Switch> <
-        /main> <
-        />
-      );
-    }
+  const changeState = async(value, id) => {
+    setBooks(currentState => {
+      return currentState.map(
+        book => book.id === id ? {...book, currentState: value } : book
+      )
+    });
 
-    export default App;
+    await fetch(`http://localhost:5000/books/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({...books.find(el => el.id === id), "currentState": value })
+    });
+  }
+
+  const updateFilterInput = (e) => {
+    setFilterInput(e.target.value);
+  }
+
+  // return (
+  //   <div className="app">
+  //     <Switch>
+  //       <Route exact path="/">
+  //         <div className="list-books">
+  //           <div className="list-books-title"><h1>MITTReads</h1></div>
+  //           <div className="list-books-content">
+  //             <div>
+  //               <div className="bookshelf">
+  //                 <h2 className="bookshelf-title">Currently Reading</h2>
+  //                 <div className="bookshelf-books">
+  //                   <ol className="books-grid">
+  //                     {books.filter(el => el.currentState === "currentlyReading").map((book, index) => (
+  //                       <Book
+  //                         currentState={book.currentState}
+  //                         title={book.title}
+  //                         authors={book.authors}
+  //                         image={book.imageLinks}
+  //                         id={book.id}
+  //                         key={index}
+  //                         changeState={changeState}
+  //                       />
+  //                     ))}
+  //                   </ol>
+  //                 </div>
+  //               </div>
+  //               <div className="bookshelf">
+  //                 <h2 className="bookshelf-title">Want To Read</h2>
+  //                 <div className="bookshelf-books">
+  //                   <ol className="books-grid">
+  //                     {books.filter(el => el.currentState === "wantToRead").map((book, index) => (
+  //                       <Book
+  //                         currentState={book.currentState}
+  //                         title={book.title}
+  //                         authors={book.authors}
+  //                         image={book.imageLinks}
+  //                         id={book.id}
+  //                         key={index}
+  //                         changeState={changeState}
+  //                       />
+  //                     ))}
+  //                   </ol>
+  //                 </div>
+  //               </div>
+  //               <div className="bookshelf">
+  //                 <h2 className="bookshelf-title">Read</h2>
+  //                 <div className="bookshelf-books">
+  //                   <ol className="books-grid">
+  //                     {books.filter(el => el.currentState === "read").map((book, index) => (
+  //                       <Book
+  //                         currentState={book.currentState}
+  //                         title={book.title}
+  //                         authors={book.authors}
+  //                         image={book.imageLinks}
+  //                         id={book.id}
+  //                         key={index}
+  //                         changeState={changeState}
+  //                       />
+  //                     ))}
+  //                   </ol>
+  //                 </div>
+  //               </div>
+  //             </div>
+  //           </div>
+  //           <div className="open-search"><Link to="/search" className='close-search'>Add a book</Link></div>
+  //         </div>
+  //       </Route>
+  //       <Route exact path="/search">
+  //         <div className="search-books">
+  //           <div className="search-books-bar">
+  //             <Link to="/" className='close-search'>Close</Link>
+  //             <div className="search-books-input-wrapper">
+  //               <input
+  //                 className='search-contacts'
+  //                 type='text'
+  //                 placeholder="Search by title or author" 
+  //                 value={filterInput}
+  //                 onChange={updateFilterInput}
+  //               />
+  //             </div>
+  //           </div>
+  //           <div className="search-books-results">
+  //             {filterInput && <div className="results-quantity">Your search returned{`${books.filter(el => el.title.toLowerCase().includes(filterInput.toLowerCase()) || el.authors.join().toLowerCase().includes(filterInput.toLowerCase())).length} `}results.</div>}
+  //             <ol className="books-grid">
+  //               {filterInput && books.filter(el => el.title.toLowerCase().includes(filterInput.toLowerCase()) || el.authors.join().toLowerCase().includes(filterInput.toLowerCase())).map((book, index) => (
+  //                 <Book
+  //                   currentState={book.currentState}
+  //                   title={book.title}
+  //                   authors={book.authors}
+  //                   image={book.imageLinks}
+  //                   id={book.id}
+  //                   key={index}
+  //                   changeState={changeState}
+  //                 />
+  //               ))}
+  //             </ol>
+  //           </div>
+  //         </div>
+  //       </Route>
+  //     </Switch>
+  //   </div>
+  // );
+}
+
+export default App;
--- src/Book.js
+++ src/Book.js
@@ -5,38 +5,30 @@ const Book = (props) => {
     props.changeState(e.target.value, props.id);
   }
 
-  return ( <
-    li >
-    <
-    div className = "book" >
-    <
-    div className = "book-top" >
-    <
-    div className = "book-cover"
-    style = {
-      { width: "128px", height: "193px", backgroundImage: `url(${props.image.thumbnail})` } } >
-    <
-    /div> <
-    div className = "book-shelf-changer" >
-    <
-    select defaultValue = { "none" }
-    value = { props.currentState }
-    onChange = { hirad } >
-    <
-    option value = "move"
-    disabled > Move to... < /option> <
-    option value = "currentlyReading" > Currently Reading < /option> <
-    option value = "wantToRead" > Want to Read < /option> <
-    option value = "read" > Read < /option> <
-    option value = "none" > None < /option> <
-    /select> <
-    /div> <
-    /div> <
-    div className = "book-title" > { props.title } < /div> <
-    div className = "book-authors" > { props.authors.join(', ') } < /div> <
-    /div> <
-    /li>
-  );
+  // return (
+  //   <li>
+  //     <div className="book">
+  //       <div className="book-top">
+  //         <div
+  //           className="book-cover"
+  //           style={{width: "128px", height: "193px", backgroundImage: `url(${props.image.thumbnail})`}}
+  //         >
+  //         </div>
+  //         <div className="book-shelf-changer">
+  //           <select defaultValue= {"none"} value={props.currentState} onChange={hirad}>
+  //             <option value="move" disabled>Move to...</option>
+  //             <option value="currentlyReading">Currently Reading</option>
+  //             <option value="wantToRead">Want to Read</option>
+  //             <option value="read">Read</option>
+  //             <option value="none">None</option>
+  //           </select>
+  //         </div>
+  //       </div>
+  //       <div className="book-title">{props.title}</div>
+  //       <div className="book-authors">{props.authors.join(', ')}</div>
+  //     </div>
+  //   </li>
+  // );
 }
 
 export default Book;
